d9b177613e246fb95744739a5453f1910c680038
charlespasiak <charlesspasiak@gmail.com>
2023-09-11 12:00:09 +0700

    environment setup & featured products

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
 export const revalidate = 0;
 
-const HomePage = () => {
-  return <div>Hello Store</div>;
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard('09cec93f-eead-49bc-8919-083c8d0ed902')
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />      
+        <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+          <ProductList title='Featured Products' items={products} />
+        </div>
+      </div>
+    </Container>
+  )
 };
 export default HomePage;
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ className, children, disabled, type = "button" }, ref) => {
   return (
-    <button ref={ref} className={cn('w-auto rounded-full bg-black border-transparent px-5 px-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition', className)}>
+    <button ref={ref} className={cn('w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition', className)}>
       {children}
     </button>
   )
