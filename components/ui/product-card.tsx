e43364b4429ac68b57132274ebbf2d1cf83c838f
charlespasiak <charlesspasiak@gmail.com>
2023-09-13 13:51:54 +0700

    add to cart functionality

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -9,12 +9,14 @@ import { Product } from '@/types';
 import IconButton from './icon-button';
 import Currency from './currency';
 import usePreviewModal from '@/hooks/use-preview-modal';
+import useCart from '@/hooks/use-cart';
 
 interface Props {
   data: Product;
 }
 
 const ProductCard = ({ data }: Props) => {
+  const cart = useCart();
   const router = useRouter();
   const previewModal = usePreviewModal();
 
@@ -27,6 +29,12 @@ const ProductCard = ({ data }: Props) => {
 
     previewModal.onOpen(data);
   }
+  
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
+  }
 
   return (
     <div onClick={handleClick} className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'>
@@ -36,7 +44,7 @@ const ProductCard = ({ data }: Props) => {
         <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
           <div className='flex gap-x-6 justify-center'>
             <IconButton onClick={onPreview} icon={<Expand size={20} className='text-gray-600' />} />
-            <IconButton onClick={() => {}} icon={<ShoppingCart size={20} className='text-gray-600' />} />
+            <IconButton onClick={onAddToCart} icon={<ShoppingCart size={20} className='text-gray-600' />} />
           </div>
         </div>
       </div>
